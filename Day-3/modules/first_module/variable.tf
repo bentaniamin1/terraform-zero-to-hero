d2984cb4a024f1e742de56ecc9965783c8fbd9a0
Bentani Amin <78359840+bentaniamin1@users.noreply.github.com>
2024-10-20 22:50:36 +0000

    init: my module

---
+++ Day-3/modules/first_module/variable.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+    description = "ami value"
+    type = string
+  
+}
+
+variable "instance_type" {
+    description = "type of instance"
+    type = string
+}
+
+variable "subnet_id_value" {
+    description = "id of subnet"
+    type = string 
+}
